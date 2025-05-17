021cdb20173ec502af9700d8bd68ae586abfa12a
Bruno Gomes <bruno.rodrigues.gomes@hotmail.com>
2025-05-17 18:41:56 -0300

    Criação do primeiro POST que calcula o CDI

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
 import express from "express";
-import cors from 'cors'
-import pkg from '@prisma/client';
+import cors from "cors";
+import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 //alterar aqui quais paginas podem acessar meu back-end
-app.use(cors())
+app.use(cors());
 
-// app.post('/usuarios', async (req, res) =>{
+app.post("/calcular-cdi", (req, res) => {
+    const { saldoInicial, aporteMensal, tempoMeses } = req.body;
+    const taxaCDI = 0.009;
 
-//   await prisma.user.create({
-//         data:{
-//             email: req.body.email,
-//             name: req.body.name,
-//             age: req.body.age,
-//             city: req.body.city,
-//             fav: req.body.fav || false,
-//         }
-//     })
+    let saldo = saldoInicial;
+    const resultado = [];
 
-//     res.status(201).json(req.body)
-// })
+    //loop for que calcula até a quantidade de meses que o usuário pediu
+    for (let mes = 1; mes <= tempoMeses; mes++) {
+      saldo = saldo * (1 + taxaCDI) + aporteMensal;
+      resultado.push({ mes, saldo: saldo.toFixed(2) });
+    }
+    res.json(resultado);
+  });
+
+app.post("/usuario", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      nome: req.body.nome,
+      email: req.body.email,
+      createdAt: req.body.createdAt,
+    },
+  });
+  res.status(201).json(req.body);
+});
 
 app.get("/usuario", async (req, res) => {
+  const users = await prisma.user.findMany();
 
-  const users = await prisma.user.findMany()
-
-
-    res.status(200).json(users)
+  res.status(200).json(users);
 });
 
 // app.put('/usuarios/:id', async (req, res) =>{
@@ -45,10 +54,10 @@ app.get("/usuario", async (req, res) => {
 //               name: req.body.name,
 //               age: req.body.age,
 //               city: req.body.city,
-//               fav: req.body.fav 
+//               fav: req.body.fav
 //           }
 //       })
-  
+
 //       res.status(201).json(req.body)
 //   })
 
@@ -62,8 +71,7 @@ app.get("/usuario", async (req, res) => {
 //     res.status(200).json({message: 'Usuário deletado com sucesso'})
 //   })
 
-
-app.listen(3000)
+app.listen(4000);
 
 // As rotas precisam de 2 coisas:
 // 1- o Tipo da Rota (Metodo HTTTP )
@@ -74,7 +82,3 @@ app.listen(3000)
 // -Listar todos os usuários
 // -Editar um usuario
 // -Deletar um usuário
-
-
-// usuario: bruno
-// senha: zRzGJvXOYl0kFbZl
